d63c7073480cf84b7c31565bbbec8088bd921a74
doredxtheo-create <doredxtheo@gmail.com>
2026-07-05 21:39:23 +0530

    leg

---
+++ Birthday_Printer/birthday_printer.py
@@ -0,0 +1,19 @@
+
+# This is a simple birthday printer program that is made in Python.
+
+# The Students Are Represented as Student_1, Student_2 with their BirthDay Dates and so on.
+
+# Lets Take the First Students Birthday Date as 14th Of July 2013 Student 2 being 15th of August 2013 and Student 3 being 16th September 2013 and Student 4 being 17th October 2013 and Student 5 being 18th November 2013 and Student 6 being 19th December 2013 .
+
+student_1 = "14th July 2013"
+student_2 = "15th August 2013"
+student_3 = "16th September 2013"
+student_4 = "17th October 2013"
+student_5 = "18th November 2013"
+student_6 = "19th December 2013"
+
+# Lets Print The Students Birthday Dates
+
+print("The Birthday Date of Student 1 is: ", student_1, "\nThe Birthday Date of Student 2 is: ", student_2, "\nThe Birthday Date of Student 3 is: ", student_3, "\nThe Birthday Date of Student 4 is: ", student_4, "\nThe Birthday Date of Student 5 is: ", student_5, "\nThe Birthday Date of Student 6 is: ", student_6)
+
+exit(0)
