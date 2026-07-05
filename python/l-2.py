12e3b7a2c20787b4fedb290dd7b1bacebd7d0643
doredxtheo-create <doredxtheo@gmail.com>
2026-07-05 21:13:56 +0530

    leg

---
+++ python/l-2.py
@@ -0,0 +1,61 @@
+# This is a Print Statement, Which is Used to Print the Output on the Screen.
+a = "This Is My First Python Project Submitted To Codingal.com"
+print(a)
+
+# Identifiers are the names given to variables, functions, classes, etc. in Python. They are used to identify and refer to these entities in the code.
+
+# For Example: Student_Name = "Ali", I this case the student name is an identifier, And Can Start With a letter or an underscore, and can contain letters, digits, and underscores. However, they cannot start with a digit or contain any special characters or spaces.
+
+# Keywords are reserved words in Python that have a predefined meaning and cannot be used as identifiers. They are part of the Python language syntax and serve specific purposes. Examples of keywords include if, else, for, while, def, class, return, import, and many more.
+
+# For Example: if, else, for, while, def, class, return, import, and many more.
+
+# Variables are used to store data or values in a program. They act as containers that hold information that can be manipulated and accessed throughout the code.
+
+# Although They ( Variables and Indentifiers ) are similar, But they are not the same. Variables are used to store data, while identifiers are used to name and refer to those variables.
+
+# Assignment-> 1
+
+x = 5
+y = "Jhon"
+
+print(x, y)
+
+# Assignment-> 2
+
+ask_name = input("Enter Your Name: ")
+print("\nHello, " + ask_name + ", Welcome to Codingal.com!")
+
+# Assignment-> 3
+
+ask_fav_color = input("Enter Your Favorite Color: ")
+print("\nYour Favorite Color is: " + ask_fav_color)
+
+# Assignment-> 4
+
+ask_age = input("Enter Your Age ( As You Already Wrote Your Name ): ")
+print("\nYour Name Is " + ask_name + " and Your Age Is: " + ask_age)
+
+# Assignment-> 5
+
+import keyword
+
+print("\nList of Python Keywords:")
+print(keyword.kwlist)
+
+# Assignment-> 6
+
+name = input("\nEnter Your Name: ")
+age = input("Enter Your Age: ")
+country = input("Enter Your Country: ")
+
+print("\n Your Name Is: ", name, "\n Your Age Is: ", age, "\n Your Country Is: ", country)
+
+# Assignment-> 7
+
+student_name = "Ali"
+age = 12
+student_name = "Ahmed"
+print("\n The Name Of The Student Is:", student_name, "\n The Age Of The Student Is:", age)
+
+
