43a2d267df306cb665a1f7b950610facd8e38693
doredxtheo-create <doredxtheo@gmail.com>
2026-07-18 21:57:13 +0530

    leg

---
+++ Data-Type-Molariter/The-Congrtulation.py
@@ -0,0 +1,19 @@
+# The Initial Story.
+
+# Ram is congratulating his friend for getting a gold medal in the state games. He wrote the   “Congratulations” in lower case while giving input and now wants to convert it in the upper   case. Write the program for converting the word in upper case.
+
+# Ohk, It Will Be Difficult But Lets Do It.
+
+The_Message = "congratulations"
+
+print("The Initial Message Was: ", (The_Message))
+
+print("The New And Capitalized Message.")
+
+Converted_Message = The_Message.upper()
+
+print("The Converted Message Is: ", (Converted_Message) )
+
+exit(0)
+
+# btw, i didnt type the entire story i used inspect element to get the entire story. i just typed the first line and then copied the rest of the story from inspect element. i did this because it was too long to type. i hope you understand. XD It Was Very Essential.
---
+++ Data-Type-Molariter/data-type.py
@@ -0,0 +1,28 @@
+# The Primary Objective.....
+
+#Task: 
+# 1. Take User Input of Age
+# 2. Print Age
+# 3. Check Data type of age
+# 4. Convert into str
+# 5. Check data type of age ( again )
+
+# task 1
+age = input("Enter Your Age: ")
+
+# task 2
+print("Your Age Is: ", age)
+
+# task 3
+print("The Data Type Of The Vaiable Is:", type(age))
+
+# task 4
+age = str(age)
+
+print("The New Data Type Of The Vaiable Is:", type(age))
+
+# task 5
+
+print("The Final Data Type Of The Vaiable Is:", type(age))
+
+print("Ight, Now Doing The Work On The Dashboard Then Clocking Out. i Need To Sleep. Look at The File In This Folder. See You Tommarow.")
