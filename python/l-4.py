47d08fc731b45de000c69387bf36e3b46d055880
doredxtheo-create <doredxtheo@gmail.com>
2026-07-19 21:15:29 +0530

    leg

---
+++ python/l-4.py
@@ -0,0 +1,50 @@
+# Operator.py ( Imaginary name btw)
+
+
+# List Operators
+
+operators = ['+', '-', '*', '/', '%', '**', '//']
+
+print("\nList of Operators: ", operators)
+
+
+print(2**6)
+
+
+# Est-1
+
+amount = int(input("Enter The Amount For Withdrawl jk: "))
+
+note_1 = amount // 100
+
+note_2 = ( amount % 100 ) // 50 
+
+note_3 = ((amount % 100) % 50)
+
+print("Notes Of 100 Bucks:", note_1)
+print("Notes of 50 Bucks:", note_2)
+print("Notes of 10 Bucks:", note_3)
+
+
+# Assignment 2:
+
+print("Enter The Marks Obtained In Four Subjects")
+
+math = 40
+
+hindi = 28
+
+english = 80
+
+total = (math+hindi+english)
+
+percentage = ( total / 300 ) * 100
+
+print(total)
+
+print(percentage)
+
+avrage = ( total / 3 )
+
+print(avrage)
+
