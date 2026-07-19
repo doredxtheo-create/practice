5091fe3a3c10b68a761f050cfd8965d83c946ffa
doredxtheo-create <doredxtheo@gmail.com>
2026-07-19 21:50:15 +0530

    checkmate

---
+++ The-Mashallah-Square-Rooter/square-rooter.py
@@ -0,0 +1,16 @@
+# The Initial Story
+
+import math
+
+# Shiya is facing a lot of problems while calculating the square root. So she wants to design a program that will help her find the square root of whatever number she wants. So write a program to find the Square root of the number given by the user.
+
+# Lets Take The /Usr/ Input For Any "Square Root" That They Are Having An Personal Problem With.....
+
+input_for_square = int(input("Enter The Target: "))       # A Clear Advanced Techneique Of Soucrecy       Called                     TypeCasting...
+
+x = math.sqrt(input_for_square)
+
+print(x)
+
+print("CHECKMATE")
+exit(0)
