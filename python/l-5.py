d0afbc638d5faf47d4d3cbe54f50d2ad29dc8931
doredxtheo-create <doredxtheo@gmail.com>
2026-07-25 21:17:45 +0530

    leg

--- python/l-5.py
+++ python/l-5.py
@@ -1,4 +1,4 @@
-# Assigment 1 -> This is Inevitability
+# assigment 1 -> this is inevitability
 
 a = "a"
 b = a
@@ -11,26 +11,55 @@ rain = True
 if rain == True:
     print("take an umbrella")
 
-# Assignmemt 2 ->
+# assignmemt 2 ->
 
-temp_input = int(input("Enter Todays Temprature: "))
+temp_input = int(input("enter todays temprature: "))
 
 if temp_input < 20:
     outfit = "jacket"
-    print("It Is Very Cold Today....")
-    print("Wear A ", outfit, " Today")
+    print("it is very cold today....")
+    print("wear a ", outfit, " today")
 else:
-    outfit = "T-Shirt"
-    print("It is normally warm today")
-    print("Wear a "+outfit+" Today")
+    outfit = "t-shirt"
+    print("it is normally warm today")
+    print("wear a "+outfit+" today")
 
-# Assigment 3 ->
+# assigment 3 ->
 
-check_rain = input("Is it raining Today? (yes/no) ")
+check_rain = input("is it raining today? (yes/no) ")
 if check_rain == "yes":
     print("take an umbrella")
 else:
     print("leg")
     
+input_int = int(input("enter an integer: "))
 
+if input_int % 2 == 0:  # if return = 0 then even else odd -32
+    print("the integer is even")
+else:
+    print("the integer is odd")
+
+
+
+num = int(input("enter the integer for correction: "))
+
+if num > 0: # if num = positive print noice else not noice
+    print("noice :)")
+else:
+    print("not noice :( ")
+
+# aight bet
+
+
+ask_cp = int(input("Enter The Price From Which You have bought it from the store ( i dont care where you got it from ): "))
+
+ask_sp = int(input("How much did you sell it for? "))
+
+# Differeciation
+
+if ask_cp > ask_sp:
+    print("Then Learn how to do buissness you had a loss ")
+else:
+    profit = ask_sp - ask_cp
+    print("Im Happy That You Did Something Correct For Once. You Gained A profit of: ",profit)
 
