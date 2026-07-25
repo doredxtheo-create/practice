23b922a104caa3fa7e1f019946523cd0839716f7
doredxtheo-create <doredxtheo@gmail.com>
2026-07-25 20:58:33 +0530

    leg

--- python/l-5.py
+++ python/l-5.py
@@ -9,4 +9,28 @@ if a == b:
 rain = True
 
 if rain == True:
-    print("take ann umbrella")
+    print("take an umbrella")
+
+# Assignmemt 2 ->
+
+temp_input = int(input("Enter Todays Temprature: "))
+
+if temp_input < 20:
+    outfit = "jacket"
+    print("It Is Very Cold Today....")
+    print("Wear A ", outfit, " Today")
+else:
+    outfit = "T-Shirt"
+    print("It is normally warm today")
+    print("Wear a "+outfit+" Today")
+
+# Assigment 3 ->
+
+check_rain = input("Is it raining Today? (yes/no) ")
+if check_rain == "yes":
+    print("take an umbrella")
+else:
+    print("leg")
+    
+
+
