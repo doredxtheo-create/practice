0bca4d8539417d0c8fd08978c18c5ed3bc274352
doredxtheo-create <doredxtheo@gmail.com>
2026-07-26 21:15:08 +0530

    leg

--- Summer_Times/summer_times.py
+++ Summer_Times/summer_times.py
@@ -17,3 +17,4 @@ else:
     outfit = "heavy jacket"
 
 print("Recommended outfit:", outfit)
+
---
+++ python/l-6.py
@@ -0,0 +1,37 @@
+# elif else etc
+
+x = "Rohan"
+y = "Gukesh"
+z = x+y
+if x + y == z or z == x+y:
+    print("True")
+
+# Assignment 1 _>
+
+input_age = int(input("Please Enter Your Age: "))
+ticket = True
+
+if input_age>= 12 and ticket:
+    print("Enjoy The RollerCoaster Today!")
+
+elif input_age>= 12 and not ticket:
+    print("Go Buy A Ticket Please!")
+
+else:
+    print("You Are Very Young. You Will get scared.")
+
+# Assigment -3
+
+a = 10
+b = 12
+c = 12
+
+print( a == b )
+
+if a == b:
+   print("True")
+
+# Reverse
+
+if not( a == b ):
+    print("True")
