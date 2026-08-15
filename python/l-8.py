b879aada9c2c71591a8d0413c1c8efcf9d970205
doredxtheo-create <doredxtheo@gmail.com>
2026-08-15 13:13:12 +0530

    leg

--- python/l-8.py
+++ python/l-8.py
@@ -1,17 +1,17 @@
-# For Example: (Assignment 1) 
+# For Example: (Assignment 1)
 
 n = int(input("Enter The Number whose sum whom we want to find: "))
 # 5 -> 1+2+3+4+5 = 150 ;
 sum = 0
 
-for i in range(1, n+1):
+for i in range(1, n + 1):
     sum = sum + i
 print("The sum of numbers from 1 to", n, "is:", sum)
 
 
 # Assignments 2: (String Reverse)
 
-user_input = input("Enter A String To Reverse: ") # leg
+user_input = input("Enter A String To Reverse: ")  # leg
 reverse = ""
 for char in user_input:
     reverse = char + reverse
@@ -21,10 +21,10 @@ print("The reversed string is:", reverse)
 
 n = 10
 
-for i in range(10,0,-2):
+for i in range(10, 0, -2):
     print(i)
 
-# "Take No. from user and print from that no. down to 1" -> Ass-1 
+# "Take No. from user and print from that no. down to 1" -> Ass-1
 
 # Assignment -2: Print no.s from 20 to 10 in reverse order.
 
@@ -41,5 +41,3 @@ for i in range(20, 10, -1):
     print(i)
 
 exit(0)
-
-
