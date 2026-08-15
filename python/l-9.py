caa181144827cc4e1dcb1b63b104ab81a5996b29
doredxtheo-create <doredxtheo@gmail.com>
2026-08-15 21:18:03 +0530

    leg

---
+++ python/l-9.py
@@ -0,0 +1,15 @@
+
+# Assignment: 1
+ 
+
+terms = int(input("Enter The Number Of Terms: "))
+sum = 0
+i = 1
+while i <= terms:
+    sum += i
+    i += 1
+print("The sum of the numbers are: ", sum)
+
+# Assignment: 2
+
+number_input = int(input(": "))
