cd7b85d685569aa91075fae58e979f30b10c6151
doredxtheo-create <doredxtheo@gmail.com>
2026-08-16 21:18:44 +0530

    checkmate

--- python/l-10.py
+++ python/l-10.py
@@ -1,3 +1,17 @@
 for i in range(5):
     if i % 2 == 0:
         print(f"{i} is even")
+
+# Assignment:1
+
+string_input = input("Enter Your Word: ")
+character = input("Enter Your Character: ")
+i  = 0
+count = 0
+while i < len(string_input):
+    if string_input[i] == character:
+        count += 1
+    i += 1
+print(f"The character '{character}' appears {count} times in the word '{string_input}'.")
+
+
