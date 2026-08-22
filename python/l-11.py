d8df809e26bcb3d0579786e4a52b06d870e96c97
doredxtheo-create <doredxtheo@gmail.com>
2026-08-22 21:09:02 +0530

    leg

---
+++ python/l-11.py
@@ -0,0 +1,22 @@
+i_input = int(input("Enter The Amount Of Rows That You Require: "))
+
+# print(i_input)
+
+# for i in range(i_input):
+#     for j in range(i+1):
+#         print("*", end="")
+#     print()
+
+# Reverse Triangle
+
+# for i in range(i_input):
+#     for j in range(i_input-i):
+#         print("*")
+#     print()
+
+# Instead of Stars We Use Numbers
+
+for i in range(i_input):
+    for j in range(i+1):
+        print(j+1, end=" ")
+    print()
