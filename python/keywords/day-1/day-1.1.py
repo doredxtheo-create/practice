79d215472335f3bdf8a1bb934199bad295bbbbb7
doredxtheo-create <doredxtheo@gmail.com>
2026-09-12 20:20:42 +0530

    leg

---
+++ python/keywords/day-1/day-1.1.py
@@ -0,0 +1,47 @@
+# imaginary name Keywords.py :)
+
+# list of keywords in python
+keywords = [
+    "False",
+    "None",
+    "True",
+    "and",
+    "as",
+    "assert",
+    "async",
+    "await",
+    "break",
+    "class",
+    "continue",
+    "def",
+    "del",
+    "elif",
+    "else",
+    "except",
+    "finally",
+    "for",
+    "from",
+    "global",
+    "if",
+    "import",
+    "in",
+    "is",
+    "lambda",
+    "nonlocal",
+    "not",
+    "or",
+    "pass",
+    "raise",
+    "return",
+    "try",
+    "while",
+    "with",
+    "yield",
+]
+
+
+# I googled the keywords in python and found this list.
+
+
+
+
