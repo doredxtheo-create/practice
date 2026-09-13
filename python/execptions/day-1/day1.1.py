324c52497b13f957f9facec31c525fbec6bb1107
doredxtheo-create <doredxtheo@gmail.com>
2026-09-13 21:14:54 +0530

    leg

--- python/execptions/day-1/day1.1.py
+++ python/execptions/day-1/day1.1.py
@@ -27,3 +27,33 @@
 # num = [0]
 
 # print(num[3])
+
+
+
+
+# Assignment 1: ValueError
+
+try:
+    inp = int(input("Enter a number: "))
+# print("the Number is: "+ inp)
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
+else:
+    print("The Number is: " + str(inp))
+
+# Assignment 2: ZeroDivisionError
+try:
+    n1,n2 = eval(input("Enter two numbers: "))
+    result = n1/n2
+    print("The Result is: "+ str(result))
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+except SyntaxError:
+    print("Invalid Input Has Been Used, Please Only Use Two Numbers Separated By A Comma.")
+except:
+    print("An unexpected error occurred.")
+else:
+    print("The Result is: " + str(result))
+finally:
+    print("leg")
+
