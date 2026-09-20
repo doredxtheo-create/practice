f3ff0c900bc08f5f2a6132f6fdda457dce3e7cc4
doredxtheo-create <doredxtheo@gmail.com>
2026-09-20 21:13:51 +0530

    gel

---
+++ python/datetimes/datetimes.py
@@ -0,0 +1,25 @@
+
+import calendar
+from datetime import *
+
+today = date.today()
+
+def tday():
+    date.month
+    date.year
+    today
+
+
+datetme = datetime.now()
+
+print(today)
+print("\n",datetme)
+
+print(tday())
+
+
+# import datetime
+
+# bday = datetime.date(2015,2,1)
+
+# print(bday)
